abf8e507d8bc0d486870e9eba6a1a58f8828f439
villalb4 <villalb4angel@gmail.com>
2022-06-18 04:22:59 -0300

    finished

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -12,7 +12,7 @@ export default function Card() {
   
   function handleSubmit(e) {
     e.preventDefault()
-
+    setSubmit(true)
   }
 
   function handleRating(num) {
@@ -23,34 +23,43 @@ export default function Card() {
 
   function Send() {
     return (
-      <div>
-        <img src={hero} alt="hero" />
-        <div>
-          <span>You selected {rating} out of 5</span>
+      <div className="send">
+        <div className="div_hero">
+          <img src={hero} alt="hero" />
+        </div>
+        <div className="total">
+          <span className="starts">You selected {rating} out of 5</span>
         </div>
         <h2>Thank you!</h2>
-        <p>We appreciate you taking the time to give a ratng. If you ever need more support, don't hesitate to get in touch!</p>
+        <p className="para_thanks">We appreciate you taking the time to give a ratng. If you ever need more support, don't hesitate to get in touch!</p>
       </div>
+    )
+  }
+
+  function Component() {
+    return (
+      <>
+        <div className="div_star">
+          <img src={star} alt="star" />
+        </div>
+        <section>
+          <h2>How did we do?</h2>
+          <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offeing!</p>
+        </section>
+        <Puntos onRating={handleRating} />
+        <button 
+          className="submit" 
+          type="submit"
+          onClick={handleSubmit}
+        >SUBMIT</button>
+      </>
     )
   }
 
 
   return (
     <div className="card">
-      <div className="div_star">
-        <img src={star} alt="star" />
-      </div>
-      <section>
-        <h2>How did we do?</h2>
-        <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offeing!</p>
-      </section>
-      <Puntos onRating={handleRating} />
-      <button 
-        className="submit" 
-        type="submit"
-        onClick={handleSubmit}
-      >SUBMIT</button>
-      <Send />
+      {submit ? <Send /> : <Component />}
     </div>
   )
 }
